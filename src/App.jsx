c035cf913b0534f600e59865732ecce304b112b2
AnggiAdnanFauzi <AnggiAdnanFauzi@users.noreply.github.com>
2026-08-11 16:13:40 +0700

    Fix React Router blank screen on GitHub Pages by adding basename

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import ContactPage from './pages/ContactPage';
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<HomePage />} />
